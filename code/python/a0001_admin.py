f6de7b9032db63de406322321119a3aba293a1c0
jesyndre@gmail.com <jesyndre@gmail.com>
2022-03-10 11:20:21 -0800

    error

--- code/python/a0001_admin.py
+++ code/python/a0001_admin.py
@@ -391,7 +391,7 @@ def work_completed(task_name, task_number, complete):
 
     df.drop_duplicates()
     df = df.sort_values('number', ascending=False)
-    df = df.drop_duplicates(subset=complete, keep='first')
+    df.drop_duplicates(subset=complete, keep='first')
     df = clean_dataframe(df)
     df.to_csv(pd.read_csv(os.path.join(retrieve_path('work_plan'))))
 
