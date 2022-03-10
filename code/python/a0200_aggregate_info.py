a879c3a23dcdbc910d486f20c05f8000604926b9
jesyndre@gmail.com <jesyndre@gmail.com>
2022-03-10 15:42:55 -0800

    error

--- code/python/a0200_aggregate_info.py
+++ code/python/a0200_aggregate_info.py
@@ -52,9 +52,10 @@ def annual_count():
 
     for name_dataset in retrieve_list('name_dataset'):
 
+        work_completed('aggregate_annual_count' + '_' + name_dataset, 0)
         name_src, name_dst, name_summary, name_unique, plot_unique = name_paths('gscholar')
         print('name_dataset = ' + name_dataset)
- 
+
         for term in retrieve_list('search_terms'):
 
             try:
@@ -97,6 +98,8 @@ def annual_count():
 
             annual_plot(name_dataset, df_annual, term)
 
+        work_completed('aggregate_annual_count' + '_' + name_dataset, 1)
+ 
     work_completed('aggregate_annual_count', 1)
 
 
