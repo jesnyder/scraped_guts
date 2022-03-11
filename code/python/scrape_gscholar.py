56f73147f02bbd2c6481dcae57b6420cff2ca6ef
jesyndre@gmail.com <jesyndre@gmail.com>
2022-03-10 18:03:40 -0800

    error

--- code/python/scrape_gscholar.py
+++ code/python/scrape_gscholar.py
@@ -63,6 +63,16 @@ def scrape_gscholar_article():
     """
 
     """
+    headers = {
+        'User-agent':
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
+        }
+
+    proxies = {
+        'http': os.getenv('HTTP_PROXY') # or just type proxy here without os.getenv()
+        }
+
+
     name_dataset = 'gscholar'
     name_src, name_dst, name_summary, name_unique, plot_unique = name_paths(name_dataset)
     df_path = os.path.join(retrieve_path(name_src), 'df')
