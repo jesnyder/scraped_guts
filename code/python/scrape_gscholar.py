710bed86526fa1b2a1bded0454902f368f9c32ed
jesyndre@gmail.com <jesyndre@gmail.com>
2022-03-15 17:23:44 -0700

    error

--- code/python/scrape_gscholar.py
+++ code/python/scrape_gscholar.py
@@ -532,7 +532,8 @@ def missing_json_scraped():
         json_file = os.path.join(retrieve_path(name_src), 'json')
         if not os.path.exists(json_file):
             os.makedirs(json_file)
-            json_file = os.path.join(retrieve_path(name_src), 'json', title_short + '.json' )
+
+        json_file = os.path.join(retrieve_path(name_src), 'json', title_short + '.json' )
         json_file = open(json_file, 'w+')
         json_file.write(data_json)
         json_file.close()
@@ -560,6 +561,7 @@ def retrieve_html(url):
     html = requests.get(url, headers=headers, proxies=proxies).text
     soup = BeautifulSoup(html, 'lxml')
     return(soup)
+
 
 if __name__ == "__main__":
     main()
