91eaf67c6d11d83ceb5c96395093f033896dd225
jesyndre@gmail.com <jesyndre@gmail.com>
2022-03-16 20:29:21 -0700

    error

--- code/python/scrape_gscholar.py
+++ code/python/scrape_gscholar.py
@@ -114,9 +114,9 @@ def check_scraped(name_dataset, term, year, num):
         # find and compare file year to year passed into the function
         pattern = '[0-9]{3}'
         file_num = re.findall(pattern, file)
+        file_num = file_num[1]
         print('file_num = ')
         print(file_num)
-        file_num = file_num[1]
         if str(file_num) != str(num): continue
         print('file_num = ' + file_num + ' num = ' + str(num))
 
