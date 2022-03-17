e94aeef0cc4707f82e4faae855b26880e70ff66a
jesyndre@gmail.com <jesyndre@gmail.com>
2022-03-16 19:49:09 -0700

    error

--- code/python/scrape_gscholar.py
+++ code/python/scrape_gscholar.py
@@ -33,6 +33,13 @@ def scrape_gscholar():
     Check for redundency
     """
 
+    search_gscholar()
+    aggregate_json()
+    search_articles()
+    aggregate_df()
+
+    """
+
     # scrape json of specific publications by their title
     work_completed('acquire_gscholar_missing_json_scraped', 0)
     missing_json_scraped()
@@ -61,6 +68,212 @@ def scrape_gscholar():
     # scrape metaata for each article as json
 
     # add article metadata to df
+
+    """
+
+
+def check_scraped(name_dataset, term, year, num):
+    """
+
+    """
+    name_src, name_dst, name_summary, name_unique, plot_unique = name_paths(name_dataset)
+    src_path = retrieve_path(name_src)
+
+    paths_to_check = [src_path]
+
+    if name_dataset == 'gscholar':
+        src_path_json = os.path.join(src_path, 'json')
+        paths_to_check.append(src_path_json)
+        df_path = os.path.join(retrieve_path(str(name_dataset + '_article_df')))
+        paths_to_check.append(df_path)
+
+    for scr_path in paths_to_check:
+
+        for file in os.listdir(src_path):
+
+            # check specific gscholar search
+            file_split = file.split('.')
+            if file_split[0] == term: return(True)
+
+            # find and compare file term to term passed into the function
+            pattern = '[a-z]+'
+            flags = re.IGNORECASE
+            file_term = re.findall(pattern, file, flags)
+            file_term = file_term[0]
+            print('file_term = ' + file_term + ' term = ' + term)
+            if file_term != term: continue
+            print('file_term = ' + file_term + ' term = ' + term)
+
+            # find and compare file year to year passed into the function
+            pattern = '[0-9]{4}'
+            file_year = re.findall(pattern, file)
+            file_year = file_year[0]
+            if file_year != year: continue
+            print('file_year = ' + file_year + ' year = ' + str(year))
+
+            # find and compare file year to year passed into the function
+            pattern = '[0-9]{3}'
+            file_num = re.findall(pattern, file)
+            file_num = file_num[0]
+            if str(file_num) != str(num): continue
+            print('file_num = ' + file_num + ' num = ' + str(num))
+
+            # find and compare file saved date to current date
+            pattern = '[0-9]{4}' + '-' + '[0-9]{2}' + '-' +  '[0-9]{2}'
+            file_date_saved = re.findall(pattern, file)
+            file_date_saved = file_date_saved[0]
+            print('file_date_saved = ' + file_date_saved)
+
+            a = file_date_saved.split('-')
+            a = datetime.datetime(int(a[0]), int(a[1]), int(a[2]), 0, 0)
+            print('a = ' + str(a))
+            b = datetime.datetime.today()
+            print('b = ' + str(b))
+            v = b-a
+            print('v = ' + str(v))
+            v = int(v.days)
+            print('v = ' + str(v))
+            if v < 3:
+                print('date match: ' + str(v))
+                print('too many days lapsed since last query.')
+                return(True)
+
+    return(False)
+
+
+def search_gscholar():
+    """
+    Retrieve json year by year
+    """
+    for term in retrieve_list('search_terms'):
+
+        currentDateTime = datetime.datetime.now()
+        date = currentDateTime.date()
+
+        search_year_min = int(retrieve_format('search_year_min'))-1
+        for year in range(int(date.strftime("%Y")), search_year_min, -1):
+
+            work_completed('begin_acquire_gscholar_json_' + str(year), 0)
+            for num in np.arange(0, 50, 1, dtype=int):
+
+                num_str = str(num).zfill(3)
+                url = 'https://scholar.google.com/scholar?'
+                url = url + 'start=' + str(int(num*10))
+                url = url + '&q=' + term
+                #url = url + '&hl=en&as_sdt=0,5'
+                url = url + '&hl=en&as_sdt=0,5'
+                url = url + '&as_ylo=' + str(year)
+                url = url + '&as_yhi=' + str(year)
+
+                # check if recently scraped
+                if check_scraped('gscholar', term, year, num_str) == True:
+                    print('json found.')
+                    continue
+
+                soup = retrieve_html(url)
+                if error_check(soup) == True: return('error')
+
+                data = html_to_json(soup)
+                if data == []: break
+                if len(data) < 10 and year != int(date.strftime("%Y")):
+                    work_completed('begin_acquire_gscholar_json_' + str(year), 1)
+
+                data_json = json.dumps(data, indent = 2, ensure_ascii = False)
+                print(data_json)
+
+                name_src, name_dst, name_summary, name_unique, plot_unique = name_paths('gscholar')
+                json_file = os.path.join(retrieve_path(name_src), 'json', term + ' ' + str(year) + ' ' + str(num_str) + ' ' + str(retrieve_datetime())  + '.json' )
+                json_file = open(json_file, 'w')
+                json_file.write(data_json)
+                json_file.close()
+
+
+def html_to_json(soup):
+    """
+
+    """
+
+    # Scrape just PDF links
+    for pdf_link in soup.select('.gs_or_ggsm a'):
+        pdf_file_link = pdf_link['href']
+        print(pdf_file_link)
+
+    # JSON data will be collected here
+    data = []
+
+    # Container where all needed data is located
+    for result in soup.select('.gs_ri'):
+        title = result.select_one('.gs_rt').text
+
+        try:
+            title_link = result.select_one('.gs_rt a')['href']
+        except:
+            title_link = ''
+
+        publication_info = result.select_one('.gs_a').text
+        snippet = result.select_one('.gs_rs').text
+        cited_by = result.select_one('#gs_res_ccl_mid .gs_nph+ a')['href']
+        related_articles = result.select_one('a:nth-child(4)')['href']
+
+        # get the year of publication of each paper
+        try:
+            txt_year = result.find("div", class_="gs_a").text
+            ref_year = re.findall('[0-9]{4}', txt_year)
+            ref_year = ref_year[0]
+        except:
+            ref_year = 0
+
+        # get number of citations for each paper
+        try:
+            txt_cite = result.find("div", class_="gs_fl").find_all("a")[2].string
+            citations = txt_cite.split(' ')
+            citations = (citations[-1])
+            citations = int(citations)
+        except:
+            citations = 0
+
+        try:
+            all_article_versions = result.select_one('a~ a+ .gs_nph')['href']
+        except:
+            all_article_versions = None
+
+        data.append({
+            'year': ref_year,
+            'title': title,
+            'title_link': title_link,
+            'publication_info': publication_info,
+            'snippet': snippet,
+            'citations': citations,
+            'cited_by': f'https://scholar.google.com{cited_by}',
+            'related_articles': f'https://scholar.google.com{related_articles}',
+            'all_article_versions': f'https://scholar.google.com{all_article_versions}',
+        })
+
+    return(data)
+
+
+
+
+
+def aggregate_json():
+    """
+
+    """
+
+
+def search_articles():
+    """
+
+    """
+
+
+def aggregate_df():
+    """
+
+    """
+
+
+
 
 
 def scrape_gscholar_article():
@@ -366,75 +579,6 @@ def json_to_dataframe():
 
 
 # support programs
-
-def check_scraped(name_dataset, term, year, num):
-    """
-
-    """
-    name_src, name_dst, name_summary, name_unique, plot_unique = name_paths(name_dataset)
-    src_path = retrieve_path(name_src)
-
-    paths_to_check = [src_path]
-
-    if name_dataset == 'gscholar':
-        src_path_json = os.path.join(src_path, 'json')
-        paths_to_check.append(src_path_json)
-        df_path = os.path.join(retrieve_path(str(name_dataset + '_article_df')))
-        paths_to_check.append(df_path)
-
-    for scr_path in paths_to_check:
-
-        for file in os.listdir(src_path):
-
-            # check specific gscholar search
-            file_split = file.split('.')
-            if file_split[0] == term: return(True)
-
-            # find and compare file term to term passed into the function
-            pattern = '[a-z]+'
-            flags = re.IGNORECASE
-            file_term = re.findall(pattern, file, flags)
-            file_term = file_term[0]
-            print('file_term = ' + file_term + ' term = ' + term)
-            if file_term != term: continue
-            print('file_term = ' + file_term + ' term = ' + term)
-
-            # find and compare file year to year passed into the function
-            pattern = '[0-9]{4}'
-            file_year = re.findall(pattern, file)
-            file_year = file_year[0]
-            if file_year != year: continue
-            print('file_year = ' + file_year + ' year = ' + str(year))
-
-            # find and compare file year to year passed into the function
-            pattern = '[0-9]{3}'
-            file_num = re.findall(pattern, file)
-            file_num = file_num[0]
-            if str(file_num) != str(num): continue
-            print('file_num = ' + file_num + ' num = ' + str(num))
-
-            # find and compare file saved date to current date
-            pattern = '[0-9]{4}' + '-' + '[0-9]{2}' + '-' +  '[0-9]{2}'
-            file_date_saved = re.findall(pattern, file)
-            file_date_saved = file_date_saved[0]
-            print('file_date_saved = ' + file_date_saved)
-
-            a = file_date_saved.split('-')
-            a = datetime.datetime(int(a[0]), int(a[1]), int(a[2]), 0, 0)
-            print('a = ' + str(a))
-            b = datetime.datetime.today()
-            print('b = ' + str(b))
-            v = b-a
-            print('v = ' + str(v))
-            v = int(v.days)
-            print('v = ' + str(v))
-            if v < 3:
-                print('date match: ' + str(v))
-                print('too many days lapsed since last query.')
-                return(True)
-
-    return(False)
-
 
 def error_check(soup):
     """
