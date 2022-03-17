1e7051f369e408c61479978ee4ea03313318818c
jesyndre@gmail.com <jesyndre@gmail.com>
2022-03-17 15:25:07 -0700

    error

--- code/python/acquire_publication.py
+++ code/python/acquire_publication.py
@@ -99,6 +99,7 @@ def query_crossref():
             print(df)
 
         #df = clean_dataframe(df)
+        print(retrieve_path('crossref_df'))
         df.to_csv(retrieve_path('crossref_df'), term + '.csv')
         time.sleep(30)
         hello
