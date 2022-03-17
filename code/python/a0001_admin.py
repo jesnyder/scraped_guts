bde3cc1bd0b1bbed97ec8c79490b0daefc919c95
jesyndre@gmail.com <jesyndre@gmail.com>
2022-03-16 20:37:06 -0700

    error

--- code/python/a0001_admin.py
+++ code/python/a0001_admin.py
@@ -116,11 +116,11 @@ def retrieve_datetime():
     # datetime object containing current date and time
     now = datetime.now()
 
-    print("now =", now)
+    #print("now =", now)
 
     # dd/mm/YY H:M:S
     dt_string = now.strftime("%Y-%m-%d %H %M %S")
-    print("date and time =", dt_string)
+    #print("date and time =", dt_string)
 
     return(dt_string)
 
