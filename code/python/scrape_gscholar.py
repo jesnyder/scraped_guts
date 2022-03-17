da33256520e264759612a79b180cc960e9bbb5f8
jesyndre@gmail.com <jesyndre@gmail.com>
2022-03-16 20:54:50 -0700

    error

--- code/python/scrape_gscholar.py
+++ code/python/scrape_gscholar.py
@@ -99,7 +99,7 @@ def check_scraped(name_dataset, term, year, num):
 
     for file in os.listdir(src_path):
 
-        print('file = ' + file)
+        #print('file = ' + file)
 
         # check specific gscholar search
         file_split = file.split('.')
@@ -111,21 +111,21 @@ def check_scraped(name_dataset, term, year, num):
         file_term = re.findall(pattern, file, flags)
         file_term = file_term[0]
         if file_term != term: continue
-        print('file_term = ' + file_term + ' term = ' + term)
+        #print('file_term = ' + file_term + ' term = ' + term)
 
         # find and compare file year to year passed into the function
         pattern = '[0-9]{4}'
         file_year = re.findall(pattern, file)
         file_year = file_year[0]
         if str(file_year) != str(year): continue
-        print('file_year = ' + file_year + ' year = ' + str(year))
+        #print('file_year = ' + file_year + ' year = ' + str(year))
 
         # find and compare file year to year passed into the function
         pattern = '[0-9]{3}'
         file_num = re.findall(pattern, file)
         file_num = file_num[1]
         if str(file_num) != str(num): continue
-        print('file_num = ' + file_num + ' num = ' + str(num))
+        #print('file_num = ' + file_num + ' num = ' + str(num))
 
         # find and compare file saved date to current date
         file = file.split(' ')
@@ -133,20 +133,20 @@ def check_scraped(name_dataset, term, year, num):
         pattern = '[0-9]{4}' + '-' + '[0-9]{2}' + '-' +  '[0-9]{2}'
         file_date_saved = re.findall(pattern, file)
         file_date_saved = file_date_saved[0]
-        print('file_date_saved = ' + file_date_saved)
+        #print('file_date_saved = ' + file_date_saved)
 
         a = file_date_saved.split('-')
         a = datetime.datetime(int(a[0]), int(a[1]), int(a[2]), 0, 0)
-        print('a = ' + str(a))
+        #print('a = ' + str(a))
         b = datetime.datetime.today()
-        print('b = ' + str(b))
+        #print('b = ' + str(b))
         v = b-a
-        print('v = ' + str(v))
+        #print('v = ' + str(v))
         v = int(v.days)
-        print('v = ' + str(v))
+        #print('v = ' + str(v))
         if v < 3:
-            print('date match: ' + str(v))
-            print('too many days lapsed since last query.')
+            #print('date match: ' + str(v))
+            #print('too many days lapsed since last query.')
             return(True)
 
     return(False)
