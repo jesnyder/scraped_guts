063b84f70379bf83c0920ad5fd53d1a600a0dfae
jesyndre@gmail.com <jesyndre@gmail.com>
2022-03-17 15:41:44 -0700

    error

--- code/python/acquire_publication.py
+++ code/python/acquire_publication.py
@@ -89,19 +89,7 @@ def query_crossref():
                             key_name = str(key_name + '_' + str(item_num) + '_' + keys2[j])
                             df_doi[key_name] = [values2[j]]
 
-                            if keys2[j] == 'affiliation':
-                                w3 = values2[j]
-                                item_num_2 = 0
-                                for item2 in w3:
-
-                                    item_num_2 = item_num_2 + 1
-                                    keys3 = list(item2.keys())
-                                    values3 = list(item2.values())
-
-                                    for k in range(len(keys3)):
-                                        key_name = str(key_name + '_' + str(item_num_2) + '_' + keys3[k])
-                                        df_doi[key_name] = [values3[k]]
-
+                        
 
             print('df_doi = ')
             print(df_doi.T)
