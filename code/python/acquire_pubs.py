d53f396da63da1d7963f08ba69ff38c6af42ff0f
jesyndre@gmail.com <jesyndre@gmail.com>
2022-03-18 16:20:07 -0700

    error

--- code/python/acquire_pubs.py
+++ code/python/acquire_pubs.py
@@ -630,8 +630,8 @@ def search_crossref():
             w1 = works.doi(doi)
 
             data_json = json.dumps(w1, indent = 2, ensure_ascii = False)
-            doi_str = doi.replace('/', '_')
-            json_path = os.path.join(retrieve_path('pub_crossref_json'), doi + '.json')
+            doi_str = str(doi.replace('/', '_'))
+            json_path = os.path.join(retrieve_path('pub_crossref_json'), doi_str + '.json')
             json_file = open(json_path, 'w')
             json_file.write(data_json)
             json_file.close()
