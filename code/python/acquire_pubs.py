3d1031ffe115f2712a395845303493af6559b96a
jesyndre@gmail.com <jesyndre@gmail.com>
2022-03-17 18:02:47 -0700

    error

--- code/python/acquire_pubs.py
+++ code/python/acquire_pubs.py
@@ -108,8 +108,8 @@ def aggregate_df(save_to_file):
 
         df_all = df_all.append(df)
         df_all = clean_dataframe(df_all)
-        f = os.path.join(retrieve_path(name_src), save_to_file + '.csv')
-        df_all.to_csv()
+        f = os.path.join(retrieve_path(name_dst), save_to_file + '.csv')
+        df_all.to_csv(f)
 
 
 def check_scraped(name_dataset, term, year, num):
