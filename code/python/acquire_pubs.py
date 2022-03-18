c15fbeeb272dcedcca4ea526a8fafc95295a695f
jesyndre@gmail.com <jesyndre@gmail.com>
2022-03-17 17:57:32 -0700

    error

--- code/python/acquire_pubs.py
+++ code/python/acquire_pubs.py
@@ -99,11 +99,11 @@ def aggregate_df(save_to_file):
         src_path = os.path.join(retrieve_path('pub_web'))
     elif save_to_file == 'crossref_meta':
         src_path = os.path.join(retrieve_path('pub_crossref'))
- 
+
     print('src_path = ' + str(src_path))
     for file in os.listdir(src_path):
 
-        f = os.path.join(src_path, file)
+        f = os.path.join(retrieve_path(src_path), file)
         df = pd.read_csv(f)
         df = clean_dataframe(df)
 
