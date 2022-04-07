6f20bc85b2feac3372481464bbf6bee99c7fffa2
jesyndre@gmail.com <jesyndre@gmail.com>
2022-04-07 11:38:47 -0700

    reset

--- code/python/a0000_main.py
+++ code/python/a0000_main.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from a0001_admin import retrieve_list
 from a0100_aggregate_info import aggregate_info
-#from a0300_map_maker import map_maker
+from a0300_targetted_count import targetted_count
 
 def main():
     """
@@ -17,10 +17,10 @@ def main():
         aggregate_info(dataset)
 
         # targeted count of categories
-        #targeted_count(dataset)
+        targeted_count(dataset)
 
         # untargeted count of categories
-        #untargeted_count()
+        # untargeted_count()
 
         # map articles
         # map_maker(dataset)
