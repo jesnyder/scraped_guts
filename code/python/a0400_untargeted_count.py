79508a5dd454ed8e883a071490955dbcf299fac3
jesyndre@gmail.com <jesyndre@gmail.com>
2022-04-08 11:58:52 -0700

    reset

--- code/python/a0400_untargeted_count.py
+++ code/python/a0400_untargeted_count.py
@@ -65,11 +65,12 @@ def count_untargeted_words(dataset, df):
     print('df = ')
     print(df)
 
-    for i in range(len(list(df['ref_year']))):
 
-        str_all = ''
-        for name in df.columns:
-            print('name = ' + name)
+    str_all = ''
+    for name in df.columns:
+        print('name = ' + name)
+
+        for i in range(len(list(df['ref_year']))):
 
             value = df.loc[i,name]
             value = str(value)
@@ -80,13 +81,12 @@ def count_untargeted_words(dataset, df):
             str_all = str_all + ' '
             str_all = str_all.lower()
 
-    str_all  = str_all.lower()
 
     char_remove = ['.', ':', ';', '"', '/', '\'' , ',', '(', ')', '$', '?', '!', '<', '>']
     for char in char_remove:
         str_all  = str_all .replace(char, '')
 
-    str_all  = str_all .split(' ')
+    str_all  = str_all.split(' ')
 
     assert len(str_all) > 0
 
