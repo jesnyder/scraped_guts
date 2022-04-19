c20c45f703313ec3a2c28a01f2ccecb02987d792
jesyndre@gmail.com <jesyndre@gmail.com>
2022-04-19 14:19:04 -0700

    editted

--- code/python/a0400_untargeted_count.py
+++ code/python/a0400_untargeted_count.py
@@ -95,10 +95,8 @@ def word_count(dataset, df):
                 for char in char_remove:
                     cell_value  = cell_value.replace(char, '')
                 cell_value = cell_value.lower()
-
                 cell_value = str(cell_value)
                 str_all = str_all + cell_value
-
 
         str_all  = str_all.split(' ')
 
@@ -124,12 +122,12 @@ def word_count(dataset, df):
                     print('name = ' + name + ' multiplier = ' + str(multiplier) + ' percent complete = ' + str(round(sum(percents),3)))
                     percent_complete_threshold = percent_complete_threshold + 3
 
-                df_counts = df_counts.sort_values(by = 'count', ascending=False)
-                df_counts = clean_dataframe(df_counts)
-                file_dst = str(dataset + '_untargeted_count')
-                path_dst = os.path.join(retrieve_path(file_dst), dataset  + '.csv')
-                df_counts.to_csv(path_dst)
-                #print('saved to: ' + str(path_dst))
+        df_counts = df_counts.sort_values(by = 'count', ascending=False)
+        df_counts = clean_dataframe(df_counts)
+        file_dst = str(dataset + '_untargeted_count')
+        path_dst = os.path.join(retrieve_path(file_dst), dataset  + '.csv')
+        df_counts.to_csv(path_dst)
+        #print('saved to: ' + str(path_dst))
 
         clean_count(dataset, df_counts)      #
 
