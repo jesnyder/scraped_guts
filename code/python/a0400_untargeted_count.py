ce7f2a7b59b4b75c73904c1042ca4d22877bc11d
jesyndre@gmail.com <jesyndre@gmail.com>
2022-04-19 14:04:15 -0700

    editted

--- code/python/a0400_untargeted_count.py
+++ code/python/a0400_untargeted_count.py
@@ -64,7 +64,7 @@ def word_count(dataset, df):
     count the words in each dataset
     """
 
-    multipliers = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    multipliers = [0.05, 0.1, 0.2, 0.4, 0.8, 1.0]
 
     print('df = ')
     print(df)
