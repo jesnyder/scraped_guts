059c142945a5e592a1cbc3ef53be64cc4c1d4d47
jesyndre@gmail.com <jesyndre@gmail.com>
2022-04-20 10:59:06 -0700

    untargetted word count

--- code/python/find_lat_lon.py
+++ code/python/find_lat_lon.py
@@ -29,6 +29,8 @@ def findLatLong(addresses):
     #print('geolocating.')
     addresses_defined_file = retrieve_path('addresses_defined')
     df = pd.read_csv(addresses_defined_file)
+    print('df = ')
+    print(df)
     #df = clean_dataframe(df)
 
     for address in addresses:
