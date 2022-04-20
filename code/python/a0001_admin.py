172e436258c56d8cb82b4463c2a5324a0bb2a85e
jesyndre@gmail.com <jesyndre@gmail.com>
2022-04-20 16:33:57 -0700

    geolocated

--- code/python/a0001_admin.py
+++ code/python/a0001_admin.py
@@ -298,7 +298,7 @@ def sort_df():
         print(path)
 
         if 'blank_map' in file: continue
-        if 'name_dataset' in file: continue
+        if 'paths.csv' in file: continue
 
         try:
             df = pd.read_csv(path, sep='|')
