62bea913a64b3011ddb62468ca40252907e64ebc
jesyndre@gmail.com <jesyndre@gmail.com>
2022-04-21 13:37:02 -0700

    geolocated defined for clinical

--- code/python/a0100_aggregate_info.py
+++ code/python/a0100_aggregate_info.py
@@ -563,7 +563,9 @@ def list_clinical_trials(dataset):
                         print(url)
                         org_urls.append(url)
 
+
         str_org_urls=" ".join(str(elem) for elem in org_urls)
+        assert len(str_org_urls) > 0
         urls.append(str_org_urls)
 
 
